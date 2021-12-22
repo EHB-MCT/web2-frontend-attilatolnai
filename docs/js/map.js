f69bccf15ebab91f8952581fea3ac504f586e54b
attilatolnai <attila.tolnai@student.ehb.be>
2021-12-22 14:58:49 +0100

    Started get location
    Started getting location from user

--- docs/js/map.js
+++ docs/js/map.js
@@ -1,5 +1,13 @@
+//The token to access mapbox
 mapboxgl.accessToken = 'pk.eyJ1IjoidGlsYWRlaHVuIiwiYSI6ImNrdXdhNzczaDBlN2sydW1yYTQwMzM5MHUifQ.n9w_c_FBhfcJOR-QADQcpA';
+
+//Ask user for their permission to get their location
+navigator.geolocation.getCurrentPosition(successLocation,
+    errorLocation, {enableHighAccuracy: true})
+
+//new map being created
 var map = new mapboxgl.Map({
   container: 'map',
+  //style of map = what type of map do I want => street
   style: 'mapbox://styles/mapbox/streets-v11'
 });
