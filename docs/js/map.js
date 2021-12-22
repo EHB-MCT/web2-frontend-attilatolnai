cfc83432553a279cf0a7e2d7205845a25c68bb03
attilatolnai <attila.tolnai@student.ehb.be>
2021-12-22 15:21:56 +0100

    Accurate location test
    If the user declines to share their location, they will see a map of Brussels. If they agree, they should see a map of their location. To check if the longitude and latitude are correctly taken, they are first console logged, to avoid errors

--- docs/js/map.js
+++ docs/js/map.js
@@ -5,9 +5,23 @@ mapboxgl.accessToken = 'pk.eyJ1IjoidGlsYWRlaHVuIiwiYSI6ImNrdXdhNzczaDBlN2sydW1yY
 navigator.geolocation.getCurrentPosition(successLocation,
     errorLocation, {enableHighAccuracy: true})
 
+function successLocation(position){
+    console.log(position.coords.longitude);
+    console.log(position.coords.latitude);
+    //setupMap([position.coords.longitude, position.coords.latitude])
+}
+
+function errorLocation(){
+    setupMap([4.35, 50.84])
+}
+
 //new map being created
-var map = new mapboxgl.Map({
-  container: 'map',
-  //style of map = what type of map do I want => street
-  style: 'mapbox://styles/mapbox/streets-v11'
-});
+function setupMap(center){
+    var map = new mapboxgl.Map({
+        container: 'map',
+        //style = what type of map do I want
+        style: 'mapbox://styles/mapbox/streets-v11',
+        center: center,
+        zoom: 12
+      })
+}
