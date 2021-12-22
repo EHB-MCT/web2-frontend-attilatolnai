62cce97add1ac292b00a84cbdf133d0516015454
attilatolnai <attila.tolnai@student.ehb.be>
2021-12-22 15:31:03 +0100

    Created two maps
    Created two maps: one for the user who accepts to share their location and one for those who don't. Those who accept get a zoomed in view of their currrent location and thos who reject get a zoomed out view of Brussels

--- docs/js/map.js
+++ docs/js/map.js
@@ -6,20 +6,28 @@ navigator.geolocation.getCurrentPosition(successLocation,
     errorLocation, {enableHighAccuracy: true})
 
 function successLocation(position){
-    console.log(position.coords.longitude);
-    console.log(position.coords.latitude);
-    //setupMap([position.coords.longitude, position.coords.latitude])
+    //console.log(position.coords.longitude); TEST OK
+    //console.log(position.coords.latitude); TEST OK
+    setupMapUser([position.coords.longitude, position.coords.latitude])
 }
 
 function errorLocation(){
-    setupMap([4.35, 50.84])
+    setupMapDefault([4.35, 50.84])
 }
 
-//new map being created
-function setupMap(center){
+//new map being created for the user's location, which has a zoomed in view of their location
+function setupMapUser(center){
     var map = new mapboxgl.Map({
         container: 'map',
-        //style = what type of map do I want
+        style: 'mapbox://styles/mapbox/streets-v11',
+        center: center,
+        zoom: 18
+      })
+}
+//new map being created for the default location, which has an overview of Brussels
+function setupMapDefault(center){
+    var map = new mapboxgl.Map({
+        container: 'map',
         style: 'mapbox://styles/mapbox/streets-v11',
         center: center,
         zoom: 12
